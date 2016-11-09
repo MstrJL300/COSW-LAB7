757c6500b67b7501d96407e3a79e7f9491c90082
MstrJL300 <joslum3@icloud.com>
2016-11-09 15:45:54 -0500

    Versión terminada

--- Calculadora/app/src/main/java/com/example/jose/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/jose/calculadora/MainActivity.java
@@ -15,11 +15,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     EditText etNum2;
 
     Button btnAdd, btnSub, btnMult, btnDiv, btnPoint, btnEnt, btnC, btnElv2,
-            btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+            btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,btnsen,btncos,btntan,btnraiz;
 
     TextView tvResult;
-
-    String oper = "";
 
     float num1 = 0;
     float num2 = 0;
@@ -34,7 +32,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         btnAdd = (Button) findViewById(R.id.btnAdd);    btnSub = (Button) findViewById(R.id.btnSub);      btnMult = (Button) findViewById(R.id.btnMult);
         btnDiv = (Button) findViewById(R.id.btnDiv);    btnPoint = (Button) findViewById(R.id.btnPoint);  btnEnt = (Button) findViewById(R.id.btnEnt);
-        btnC = (Button) findViewById(R.id.btnC);        btnElv2 = (Button) findViewById(R.id.btnElv2);
+        btnC = (Button) findViewById(R.id.btnC);        btnElv2 = (Button) findViewById(R.id.btnElv2);    btnsen = (Button) findViewById(R.id.btnSen);
+        btncos = (Button) findViewById(R.id.btnCos);    btntan = (Button) findViewById(R.id.btnTan);      btnraiz = (Button) findViewById(R.id.btnRaiz);
+
 
         btn0 = (Button) findViewById(R.id.btn0);    btn1 = (Button) findViewById(R.id.btn1);    btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);    btn4 = (Button) findViewById(R.id.btn4);    btn5 = (Button) findViewById(R.id.btn5);
@@ -46,7 +46,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // set a listener
         btnAdd.setOnClickListener((View.OnClickListener) this); btnSub.setOnClickListener((View.OnClickListener) this);   btnMult.setOnClickListener((View.OnClickListener) this);
         btnDiv.setOnClickListener((View.OnClickListener) this); btnPoint.setOnClickListener((View.OnClickListener) this); btnEnt.setOnClickListener((View.OnClickListener) this);
-        btnC.setOnClickListener((View.OnClickListener) this);   btnElv2.setOnClickListener((View.OnClickListener) this);
+        btnC.setOnClickListener((View.OnClickListener) this);   btnElv2.setOnClickListener((View.OnClickListener) this);  btnsen.setOnClickListener((View.OnClickListener) this);
+        btncos.setOnClickListener((View.OnClickListener) this);btntan.setOnClickListener((View.OnClickListener) this);  btnraiz.setOnClickListener((View.OnClickListener) this);
 
         btn0.setOnClickListener((View.OnClickListener) this); btn1.setOnClickListener((View.OnClickListener) this); btn2.setOnClickListener((View.OnClickListener) this);
         btn3.setOnClickListener((View.OnClickListener) this); btn4.setOnClickListener((View.OnClickListener) this); btn5.setOnClickListener((View.OnClickListener) this);
@@ -98,8 +99,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     etNum1.setText(etNum1.getText()+".");
                 break;
             case R.id.btnC:
-                etNum1.setText("");
-                etNum2.setText("");
+                etNum1.setText(""); num1 = 0;
+                etNum2.setText(""); num2 = 0;
                 break;
             default:
                 break;
@@ -111,41 +112,35 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         // read EditText and fill variables with numbers
         num1 = Float.parseFloat(etNum1.getText().toString());
-//        num2 = Float.parseFloat(etNum2.getText().toString());
 
         // defines the button that has been clicked and performs the corresponding operation
         // write operation into oper, we will use it later for output
         switch (view.getId()) {
             case R.id.btnAdd:
-                oper = "+";
                 num1 = num2 + num1;
                 num2 = 0;
                 etNum1.setText(""+num1);
                 etNum2.setText("0");
                 break;
             case R.id.btnSub:
-                oper = "-";
                 num1 = num2 - num1;
                 num2 = 0;
                 etNum1.setText(""+num1);
                 etNum2.setText("0");
                 break;
             case R.id.btnMult:
-                oper = "*";
                 num1 = num2 * num1;
                 num2 = 0;
                 etNum1.setText(""+num1);
                 etNum2.setText("0");
                 break;
             case R.id.btnDiv:
-                oper = "/";
                 num1 = num2 / num1;
                 num2 = 0;
                 etNum1.setText(""+num1);
                 etNum2.setText("0");
                 break;
             case R.id.btnElv2:
-                oper = "/";
                 num1 = num1*num1;
                 etNum1.setText(""+num1);
                 etNum2.setText("0");
@@ -156,12 +151,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 num1 = 0;
                 etNum1.setText("");
                 break;
+            case R.id.btnSen:
+
+                etNum1.setText(Math.sin(num1)+"");
+                etNum2.setText("0");
+
+                break;
+            case R.id.btnCos:
+
+                etNum1.setText(Math.cos(num1)+"");
+                etNum2.setText("0");
+
+                break;
+            case R.id.btnTan:
+
+                etNum1.setText(Math.tan(num1)+"");
+                etNum2.setText("0");
+
+                break;
+            case R.id.btnRaiz:
+
+                etNum1.setText(Math.sqrt(num1)+"");
+                etNum2.setText("0");
+
+                break;
+
             default:
                 break;
         }
-
-        // form the output line
-//        etNum1.setText(""+num1);
-//        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 }
